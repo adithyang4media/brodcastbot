b81a4e6acc18ede9c266404e109a9526b5e3b6d9
adithyang4media <86531328+adithyang4media@users.noreply.github.com>
2021-11-14 12:56:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -124,10 +124,10 @@ def main():
     dp.add_handler(CommandHandler("hai", hai))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.all, echo))
+    dp.add_handler(MessageHandler(Filters.all, admin_handler))
     dp.add_handler(MessageHandler(Filters.photo, photo_handler))
     dp.add_handler(MessageHandler(Filters.document, file_handler))
-    dp.add_handler(MessageHandler(Filters.text, admin_handler))
+    dp.add_handler(MessageHandler(Filters.text, echo))
     
 
     # log all errors
