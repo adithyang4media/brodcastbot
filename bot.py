d038698753b588da1da1f700d943a13201902d1b
adithyang4media <86531328+adithyang4media@users.noreply.github.com>
2021-11-14 12:33:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -126,7 +126,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_handler(MessageHandler(Filters.photo, photo_handler))
     dp.add_handler(MessageHandler(Filters.document, file_handler))
-    dp.add_handler(MessageHandler(Filters.document, admin_handler))
+    dp.add_handler(MessageHandler(Filters.all, admin_handler))
     
 
     # log all errors
